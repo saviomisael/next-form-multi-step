194995b596f0ee3d1e0f858685f09721b617fc14
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-06 16:35:42 -0300

    refactor NavigationBar

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,3 +1,5 @@
+import { useAppDispatch } from '@/store/hooks/store-hooks';
+import { multiStepFormActions } from '@/store/slices/multi-step-form-slice';
 import { FC } from 'react';
 import styles from './NavigationBar.module.scss';
 
@@ -6,13 +8,31 @@ export type NavigationBarProps = {
 };
 
 export const NavigationBar: FC<NavigationBarProps> = ({ indexActive }) => {
+  const dispatch = useAppDispatch();
   const tabs = ['Detalhes', 'Preferências'];
+
+  const handleDetailsNavigation = () => {
+    dispatch(multiStepFormActions.goToDetailsStep());
+  };
+
+  const handlePreferencesNavigation = () => {
+    dispatch(multiStepFormActions.goToPreferencesStep());
+  };
+
   const tabButtonStyles = (isActive: boolean) =>
     isActive ? styles['-active'] : '';
   return (
     <nav className={styles['navigation-bar']}>
       {tabs.map((x, index) => (
         <button
+          onClick={() => {
+            const handler =
+              x === 'Detalhes'
+                ? handleDetailsNavigation
+                : handlePreferencesNavigation;
+
+            handler();
+          }}
           className={`${styles['tab-button']} ${tabButtonStyles(
             indexActive === index,
           )}`}
