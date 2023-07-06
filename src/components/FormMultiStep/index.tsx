2c41e66432375a46c284cbd6888d9e228db64a68
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-06 15:54:54 -0300

    add navigation functionality for FormMultiStep

--- src/components/FormMultiStep/index.tsx
+++ src/components/FormMultiStep/index.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useAppDispatch, useAppSelector } from '@/store/hooks/store-hooks';
+import { multiStepFormActions } from '@/store/slices/multi-step-form-slice';
+import { MouseEvent } from 'react';
 import { DetailsInputs } from '../DetailsInputs';
 import { NavigationBar } from '../NavigationBar';
 import { PreferencesBox } from '../PreferencesBox';
@@ -7,18 +10,25 @@ import { TabForm } from '../TabForm';
 import styles from './FormMultiStep.module.scss';
 
 export const FormMultiStep = () => {
-  const actualIndex: number = 1;
+  const { actualPage } = useAppSelector(({ multiStepForm }) => multiStepForm);
+  const dispatch = useAppDispatch();
+
+  const handleDetailsStep = (_: MouseEvent<HTMLButtonElement>) => {
+    dispatch(multiStepFormActions.goToPreferencesStep());
+  };
+
   return (
     <form className={styles['form-multi-step']}>
-      <NavigationBar indexActive={actualIndex} />
-      <TabForm isLastStep={false} title="Detalhes" hidden={actualIndex !== 0}>
+      <NavigationBar indexActive={actualPage} />
+      <TabForm
+        isLastStep={false}
+        title="Detalhes"
+        hidden={actualPage !== 0}
+        buttonOnClick={handleDetailsStep}
+      >
         <DetailsInputs />
       </TabForm>
-      <TabForm
-        isLastStep={true}
-        title="Preferências"
-        hidden={actualIndex !== 1}
-      >
+      <TabForm isLastStep={true} title="Preferências" hidden={actualPage !== 1}>
         <PreferencesBox />
       </TabForm>
     </form>
